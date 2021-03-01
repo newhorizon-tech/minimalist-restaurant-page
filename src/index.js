ca3d239763803c97ba97f1a91c5a5f24340b91fc
newhorizon-tech <alaukikinbox@gmail.com>
2021-03-01 19:42:52 +0530

    Add navbar
    Fix formatting

--- src/index.js
+++ src/index.js
@@ -5,22 +5,28 @@ const content = document.querySelector('#content');
 const header = document.createElement('header');
 const main = document.createElement('main');
 
-const heading = document.createElement('h1');
-heading.textContent = 'My Restaraunt';
+// const heading = document.createElement('h1');
+// heading.textContent = 'My Restaurant';
 
-header.appendChild(heading);
+// header.appendChild(heading);
+
+const nav = document.createElement('nav');
 
 const homebtn = document.createElement('button');
 const menubtn = document.createElement('button');
 const contactbtn = document.createElement('button');
 
+
+
 homebtn.textContent = "Home"
 menubtn.textContent = "Menu"
 contactbtn.textContent = "Contact"
 
-header.appendChild(homebtn);
-header.appendChild(menubtn);
-header.appendChild(contactbtn);
+nav.appendChild(homebtn);
+nav.appendChild(menubtn);
+nav.appendChild(contactbtn);
+
+header.appendChild(nav);
 
 
 content.appendChild(header);
