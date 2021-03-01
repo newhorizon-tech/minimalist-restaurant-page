db68ac7370325563a1fbe644c4b246b6f75cd4fd
newhorizon-tech <alaukikinbox@gmail.com>
2021-03-01 15:55:33 +0530

    Add basic styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 // Javascript!
-// import './src/styles.scss';
+import './styles.scss';
+const content = document.querySelector('#content');
 
-console.log("It works!")
+const header = document.createElement('header');
+const main = document.createElement('main');
 
-console.log("Yes it does!!!!");
+const heading = document.createElement('h1');
+heading.textContent = 'My Restaraunt';
+
+header.appendChild(heading);
+
+const homebtn = document.createElement('button');
+const menubtn = document.createElement('button');
+const contactbtn = document.createElement('button');
+
+homebtn.textContent = "Home"
+menubtn.textContent = "Menu"
+contactbtn.textContent = "Contact"
+
+header.appendChild(homebtn);
+header.appendChild(menubtn);
+header.appendChild(contactbtn);
+
+
+content.appendChild(header);
+content.appendChild(main);
